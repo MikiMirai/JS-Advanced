71e951ad150fae57e93fca6eead82ce6503efec9
MikiMirai <ased223@abv.bg>
2022-05-19 20:20:58 +0300

    Spacing.

--- Syntax-Func-Statements/Exercise/fruit.js
+++ Syntax-Func-Statements/Exercise/fruit.js
@@ -1,7 +1,7 @@
 function fruits(fruit, b, c)
 {
-    let sum=(b*c)/1000;
-    let weight=b/1000;
+    let sum=(b * c) / 1000;
+    let weight = b / 1000;
     console.log(`I need $${sum.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
 };
 
