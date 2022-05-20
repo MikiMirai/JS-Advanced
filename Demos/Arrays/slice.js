3b585aaf800fb0fe90dc67e1a3244d8d2753143a
MikiMirai <ased223@abv.bg>
2022-05-20 20:35:35 +0300

    Returns a part of Array by specified index range.

---
+++ Demos/Arrays/slice.js
@@ -0,0 +1,13 @@
+const myArr = [10, 20, 30, 40, 50, 60];
+
+//Get the elements from the specified index range.
+console.log(myArr.slice(0, 2));
+
+//Get the elements from the speficied index.
+console.log(myArr.slice(2));
+
+//Start from the end and get the elements with the given count.
+console.log(myArr.slice(-2));
+
+//Get the whole Array.
+console.log(myArr.slice());
