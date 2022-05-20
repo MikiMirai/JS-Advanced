cf951a55665ed19102c6498ae566d7d9a2c5074b
MikiMirai <ased223@abv.bg>
2022-05-20 20:35:00 +0300

    Find elements and returns true or false.

---
+++ Demos/Arrays/includes.js
@@ -0,0 +1,7 @@
+const myArr = [10, 20, 30, 40, 50, 60];
+
+//Returns true if the element is found.
+console.log(myArr.includes(20));
+
+//Returns true if the element is found after the specified index.
+console.log(myArr.includes(20, 1));
