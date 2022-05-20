c4e723b700a966db37ba094fb765f3abac056983
MikiMirai <ased223@abv.bg>
2022-05-20 20:35:12 +0300

    Returns index of element if found.

---
+++ Demos/Arrays/indexOf.js
@@ -0,0 +1,7 @@
+const myArr = [10, 20, 30, 40, 50, 60];
+
+//Returns index of element if found.
+console.log(myArr.indexOf(20));
+
+//Returns index of element if found after the specified index.
+console.log(myArr.indexOf(20, 1));
