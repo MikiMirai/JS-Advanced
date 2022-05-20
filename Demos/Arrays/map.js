8207562c19b3126ebc574bce1d7424807d30d433
MikiMirai <ased223@abv.bg>
2022-05-20 20:50:12 +0300

    Create a new array with the modified elements.

---
+++ Demos/Arrays/map.js
@@ -0,0 +1,7 @@
+const myArr = ['10', '20', '30', '40', '50', '60'];
+
+//Creates a new Array with the modified elements.
+const nums = myArr.map(x => Number(x));
+
+console.log(myArr);
+console.log(nums);
