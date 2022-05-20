2132c76d7e0d48b71114b01374cfd216aba0c503
MikiMirai <ased223@abv.bg>
2022-05-20 19:30:10 +0300

    Fill and reverse showcase.

---
+++ Demos/Arrays/fill-reverse.js
@@ -0,0 +1,7 @@
+const myArr = [10, 20, 30, 40, 50, 60];
+
+//Replaces the Array index 3 to index 6 with the element (7).
+console.log(myArr.fill(7, 3, 6)) 
+
+//Reverses and displays the Array.
+console.log(myArr.reverse()); 
