eab2d043dc7fe650f7cfa3d75eac3f1f98a03888
MikiMirai <ased223@abv.bg>
2022-05-26 18:12:25 +0300

    1st task done.

---
+++ Object-Composition/Lab/1.city-record.js
@@ -0,0 +1,14 @@
+function cityRecords(name, population, trasury) {
+    const result = {
+        name: name,
+        population: population,
+        trasury: trasury
+    };
+
+    return result;
+}
+
+console.log(cityRecords('Tortuga',
+    7000,
+    15000
+))
