8de2870c31ffa611fea5e94a43fc401c84d1eecf
MikiMirai <ased223@abv.bg>
2022-05-26 18:17:36 +0300

    2nd task done.

---
+++ Object-Composition/Exercise/2.construction-crew.js
@@ -0,0 +1,16 @@
+function dizziness(worker) {
+    if (worker.dizziness) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
+
+console.log(dizziness({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}
+));
