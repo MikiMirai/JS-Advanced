bad7b2dada468d16863191d823844d32a5d36315
MikiMirai <ased223@abv.bg>
2022-05-30 19:35:13 +0300

    Show or hide a paragraph with a button.

---
+++ DOM-Introduction/Exercise/03.Accordion/accordion.js
@@ -0,0 +1,12 @@
+function toggle() {
+    let extra = document.getElementById('extra');
+    let button = document.getElementsByClassName('button')[0];
+
+    if (button.textContent == 'More') {
+        button.textContent = 'Less';
+        extra.style.display = 'block';
+    } else {
+        button.textContent = 'More';
+        extra.style.display = 'none';
+    }
+}
