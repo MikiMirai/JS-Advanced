7ed098d3f72392ec02bcf7f5953e46484b243927
MikiMirai <ased223@abv.bg>
2022-05-31 20:07:52 +0300

    Find and delete item from the input.

---
+++ DOM-Manipulations-and-Events/Lab/02.Delete-From-Table/app.js
@@ -0,0 +1,15 @@
+function deleteByEmail() {
+    let value = document.querySelector('input[name="email"]').value;
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let found = false;
+    for (const row of rows) {
+        if (row.children[1].textContent == value) {
+            let parent = row.parentElement;
+            parent.removeChild(row);
+            found = true;
+        }
+    }
+
+    document.getElementById('result').textContent = found ? 'Deleted.' : 'Not found.';
+}
