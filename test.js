826a42d6469689934298b37f4d0a4938d5f1bcc6
MikiMirai <ased223@abv.bg>
2022-06-14 21:42:26 +0300

    Testing how moq works with immporting functions.

--- test.js
+++ test.js
@@ -1,3 +1,42 @@
+const { expect } = require('chai');
+const { rgbToHexColor } = require('./Unit-Testing-Error-Handling/Lab/6.rgb/rbg-to-hex');
+
+describe('RGBtoHexColor', () => {
+    it('converts black', () => {
+        expect(rgbToHexColor(0, 0, 0)).to.equal('#000000');
+    });
+
+    it('converts white', () => {
+        expect(rgbToHexColor(255, 255, 255)).to.equal('#FFFFFF');
+    });
+
+    it('converts blue to #234465', () => {
+        expect(rgbToHexColor(35, 68, 101)).to.equal('#234465');
+    });
+
+    it('converts 15, 15, 15 to #0F0F0F', () => {
+        expect(rgbToHexColor(15, 15, 15)).to.equal('#0F0F0F');
+    });
+
+    it('return undefined for missing params', () => {
+        expect(rgbToHexColor(0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0)).to.be.undefined;
+        expect(rgbToHexColor()).to.be.undefined;
+    });
+
+    it('return undefined for out of bounds', () => {
+        expect(rgbToHexColor(-1, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, -1, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, -1)).to.be.undefined;
+    });
+
+    it('return undefined for floats', () => {
+        expect(rgbToHexColor(1.1, 0, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 1.1, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, 1.1)).to.be.undefined;
+    });
+});
+
 describe('Test Suite', () => {
     it('test 1', () => {
         console.log('first test')
